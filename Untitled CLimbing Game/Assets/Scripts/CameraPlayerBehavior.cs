e96be74e59bd9d2b63c7ef789cdad181dda37995
dunteyr <39315568+dunteyr@users.noreply.github.com>
2021-12-14 20:25:36 -0700

    Squashed commit of the following:
    commit bec39b3925252c2af92db4d2c2979bd723d7e54f
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 19:31:03 2021 -0700
    
        Camera prefab
    
        created a prefab for the new camera and added them to the other scenes
    
    commit c72e999dd062b2e5e9d30e770c660a1bd873746f
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 19:11:05 2021 -0700
    
        Max camera distance and tweaks
    
        camera wont zoom out further than the specified distance. Other variables were tweaked to make the camera movement feel better
    
    commit d97b9599140d830bf1b2b786615d8dcfa75c0a1a
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 18:45:56 2021 -0700
    
        Smoothed camera distance
    
        Mathf.SmoothDamp is used to smooth the camera movement. Now a max camera distance needs to be set and some settings need to be tweaked
    
    commit b4668f41badd154ebf755c1bbf02d98625a70a1d
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 15:21:15 2021 -0700
    
        working on smoothing
    
        made a function for smoothing the camera distance because it is jerky without it. Not working yet
    
    commit be346170e478476ebb50600e29d9742d1e148ab5
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 14:24:19 2021 -0700
    
        script controls camera distance from player
    
        script is controlling camera distance  based on player speed successfully but jankily and with many bugs.
    
    commit 550792ed4c4454838286890bf6937d53b268d359
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 01:16:22 2021 -0700
    
        Small Commit
    
        Commented out the camera code that was used before cinemachine
    
    commit 48f1361d514c1e454eec6eea8fa1ff4b53f5e965
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 00:37:13 2021 -0700
    
        Cinemachine camera setup
    
        camera is zoomed out a bit, cinemachine is attached to player
    
    commit e281421a51587807a48be9ee38c504cb873ff87f
    Author: dunteyr <39315568+dunteyr@users.noreply.github.com>
    Date:   Tue Dec 14 00:19:30 2021 -0700
    
        Imported Cinemachine
    
        installed cinemachine from unity package manager

--- Untitled CLimbing Game/Assets/Scripts/CameraPlayerBehavior.cs
+++ Untitled CLimbing Game/Assets/Scripts/CameraPlayerBehavior.cs
@@ -1,34 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraPlayerBehavior : MonoBehaviour
 {
     public AbilityMenuScript abilityMenu;
 
-    public Camera mainCamera;
-    private Vector3 cameraPosition;
-    private Vector3 newCameraPosition;
+    public CinemachineBrain cameraBrain;
+    public CinemachineVirtualCamera virtualCamera;
+    public LensSettings currentLensSettings;
+    public LensSettings newLensSettings;
+    private Rigidbody2D player;
 
-    private Vector3 playerPosition;
+    [SerializeField] private float defaultOrthoSize = 9;
+    [SerializeField] private float maxOrthoSize = 15;
+    private float orthoSize;
+    [SerializeField] private float orthoAmount = 0.3f;
+    [SerializeField] private float orthoTriggerPoint = 14;
+    private float newOrthoSize;
+
+    [SerializeField] private float smoothTime = 1f;
+    float yVelocity = 0.0f;
+
+    private float playerVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        cameraBrain = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineBrain>();
+        virtualCamera = GameObject.FindWithTag("Vcam1").GetComponent<CinemachineVirtualCamera>();
+        currentLensSettings = virtualCamera.m_Lens;
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = transform.position;
-        cameraPosition = mainCamera.transform.position;
-
-        newCameraPosition.Set(playerPosition.x, playerPosition.y, cameraPosition.z);
-
-        cameraPosition = newCameraPosition;
-
-        mainCamera.transform.position = newCameraPosition;
-
+        ControlOrthoSize();
     }
+
+    //controls distance of camera from player based on player velocity
+    private void ControlOrthoSize()
+    {
+        currentLensSettings = virtualCamera.m_Lens;
+        //get player velocity and orthographic size of the virtual camera
+        playerVelocity = player.velocity.magnitude;
+        orthoSize = currentLensSettings.OrthographicSize;
+
+        //at a certain velocity, start changing ortho size
+        if(playerVelocity >= orthoTriggerPoint)
+        {
+            //add player velocity * a changeable amount constant to ortho
+            newOrthoSize = orthoSize + playerVelocity * orthoAmount;
+
+            //make sure new ortho doesnt go over the max
+            if(newOrthoSize >= maxOrthoSize)
+            {
+                newOrthoSize = maxOrthoSize;
+            }
+        }
+
+        //below the trigger velocity, target ortho size is set to the default
+        else if(playerVelocity < orthoTriggerPoint)
+        {
+            newOrthoSize = defaultOrthoSize;
+        }
+
+        //after newOrthoSize is found, give the camera new lens settings that have the smoothed new ortho size
+        virtualCamera.m_Lens = CreateLensSettings();
+    }
+
+    //function that returns LensSettings after setting all values
+    private LensSettings CreateLensSettings()
+    {
+        newLensSettings = new LensSettings(
+            currentLensSettings.FieldOfView,
+            //smooths the transition to new ortho size
+            Mathf.SmoothDamp(orthoSize, newOrthoSize, ref yVelocity, smoothTime),
+            currentLensSettings.NearClipPlane,
+            currentLensSettings.FarClipPlane,
+            currentLensSettings.Dutch);
+
+        return newLensSettings;
+    }
+
 }
