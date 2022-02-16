84edae3f9796257d074836e9064232e63fc899fe
dunteyr <39315568+dunteyr@users.noreply.github.com>
2022-02-16 11:51:06 -0700

    New background parallax works
    parallax works but the layers dont repeat and the farthest mountain doesnt move because of an obvious bug.

--- Untitled CLimbing Game/Assets/Scripts/Parallax.cs
+++ Untitled CLimbing Game/Assets/Scripts/Parallax.cs
@@ -4,31 +4,46 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    private GameObject player;
     private GameObject mainCamera;
     private GameObject farMountains;
     private GameObject closeMountains;
+
+    public GameObject background;
+    public SpriteRenderer[] layers;
 
     private Vector3 camCurrentPos;
     private Vector3 camPrevPos;
     private Vector3 camPosDiff;
 
-    [SerializeField] public float closeMountSpd = 0.0225f;
-    [SerializeField] public float farMountSpd = 0.01125f;
+    private float closeMountSpd = 0.0225f;
+    private float farMountSpd = 0.01125f;
+
+    [SerializeField]
+    private float closestLayerSpd = 0.5f;
+    [SerializeField]
+    private float farthestLayerSpd = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
 
         //playerStartPos = player.transform.position;
         farMountains = GameObject.FindGameObjectWithTag("FarMountains");
         closeMountains = GameObject.FindGameObjectWithTag("CloseMountains");
+
+        background = GameObject.FindGameObjectWithTag("Background");
+        layers = background.GetComponentsInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        ParallaxEffect();
+        //OldParallax();
+    }
+
+    void OldParallax()
     {
         //find amount of movement based on player position and previous player position
         camCurrentPos = mainCamera.transform.position;
@@ -41,6 +56,35 @@ public class Parallax : MonoBehaviour
         //vertical movement
         closeMountains.transform.position -= new Vector3(0, camPosDiff.y * closeMountSpd, 0);
         farMountains.transform.position -= new Vector3(0, camPosDiff.y * farMountSpd, 0);
+
+        //set the previous position for the next update to use
+        camPrevPos = camCurrentPos;
+    }
+
+    private void ParallaxEffect()
+    {
+        //find amount of movement based on cam position and previous cam position
+        camCurrentPos = mainCamera.transform.position;
+        camPosDiff = camCurrentPos - camPrevPos;
+
+        float layerSpdDiff = closestLayerSpd - farthestLayerSpd; //diff between fastest and slowest layer speed
+
+        float speedIncrement = layerSpdDiff / layers.Length; //increments of speed for layers. So farthest layer is slowest, next layer has 1 increment,
+                                                             //then 2 increments, and so on to fastest layer
+
+        for (int i = 0; i < layers.Length; i++)
+        {
+            //the first layer in the array is the sky which shouldnt move
+            if(i != 0)
+            {
+                //numer of increments to add for each layer. Layer 1 needs 0 increments, layer 2 needs 1 increment, etc...
+                float layerIncrement = speedIncrement * (i - 1);
+
+                //layer movement. each layer is moved based on the camera movement, the fastest and slowest speed, and layer position
+                layers[i].transform.position -= new Vector3(camPosDiff.x * farthestLayerSpd * layerIncrement, camPosDiff.y * farthestLayerSpd * layerIncrement, 0);
+            }
+   
+        }
 
         //set the previous position for the next update to use
         camPrevPos = camCurrentPos;
